3db29c4390ddd57a3834bca64b5cbbc5440de50d
Ricardo Lence Largo <lencericardo@gmail.com>
2024-03-19 18:52:07 +0100

    init commit

---
+++ src/main/java/com/plazi/market/PlaziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.plazi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaziMarketApplication.class, args);
+	}
+
+}
